a4d7be36bacf33f4c3f417eb8866c005ab5a2651
Samir2022nafie <149320386+Samir2022nafie@users.noreply.github.com>
2023-11-15 07:14:29 +0300

    Add files via upload

---
+++ ETS1189_Samir_Nafie/Power.cpp
@@ -0,0 +1,16 @@
+//Power Function Calculator
+#include<iostream>
+#include<ctype.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+float X,Y;
+    cout<<"Please Enter the First Number"<< endl;
+    cin>>X;
+    cout<<"Please Enter the Second Number"<< endl;
+    cin>>Y;
+    cout<<pow(X,Y)<< endl;
+    return 0;
+}
