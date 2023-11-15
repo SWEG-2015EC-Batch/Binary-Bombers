4c8fb62967cd82b298c3e50dbc944c96cbf80682
Samir2022nafie <149320386+Samir2022nafie@users.noreply.github.com>
2023-11-15 07:04:44 +0300

    Add files via upload

---
+++ ETS1189_Samir_Nafie/BMI_Final.cpp
@@ -0,0 +1,18 @@
+//Body Mass Index Calculator
+#include<iostream>
+#include<ctype.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    float Height, Weight, BMI;
+    cout<<"Please Enter your Height"<< endl;
+    cin>>Height;
+    cout<<"Please Enter your Weight"<< endl;
+    cin>>Weight;
+    BMI = Weight/(Height*Height);
+    cout<<"Your BMI is"<< endl;
+    cout<<BMI;
+    return 0;
+}
