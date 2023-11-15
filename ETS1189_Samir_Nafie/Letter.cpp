ba4e79bbc2979c0ad9b57e539bb87b75510d6656
Samir2022nafie <149320386+Samir2022nafie@users.noreply.github.com>
2023-11-15 07:11:03 +0300

    Add files via upload

---
+++ ETS1189_Samir_Nafie/Letter.cpp
@@ -0,0 +1,28 @@
+//Uppercase/Lowercase Letter Convertor
+#include<iostream>
+#include<ctype.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char Letter;
+    cout<<"Enter the Letter"<< endl;
+    cin>>Letter;
+    if (islower (Letter))
+        {
+            Letter = toupper(Letter);
+            cout<<Letter;
+        }
+    else if (isupper(Letter))
+		{
+		Letter = tolower(Letter);
+		cout<<Letter;
+		}
+	else 
+		{
+        cout << "The character " << Letter << " is not a letter."<< endl;
+    	}
+
+    return 0;
+}
