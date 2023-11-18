31dca88929eddfe021dcc7548f098a90ff748dae
YohannesGezahegn <149233041+YohannesGezahegn@users.noreply.github.com>
2023-11-18 09:48:33 +0300

    Update question 2.cpp

--- ETS1437_15_Yohannes_Gezahegn/question 2.cpp
+++ ETS1437_15_Yohannes_Gezahegn/question 2.cpp
@@ -1,1 +1,13 @@
 
+#include<iostream>
+using namespace std;
+int main(){
+float fuel_tank_capacity,fuel_consumption,miles_on_full_tank;
+cout<<"enter the fuel tank capacity of the automobile "<<endl;
+cin>>fuel_tank_capacity;
+cout<<"enter the fuel consumption of the automobile"<<endl;
+cin>>fuel_consumption;
+miles_on_full_tank = fuel_consumption*fuel_tank_capacity;
+cout<<"the maximum miles on full tank of this car = "<<miles_on_full_tank<<" miles "<<endl;
+return 0;
+}
