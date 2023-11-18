731b08124753ea903c480d5f121ccd57e962f99e
YohannesGezahegn <149233041+YohannesGezahegn@users.noreply.github.com>
2023-11-18 09:51:51 +0300

    Update question 3.cpp

--- ETS1437_15_Yohannes_Gezahegn/question 3.cpp
+++ ETS1437_15_Yohannes_Gezahegn/question 3.cpp
@@ -1,1 +1,23 @@
 
+
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main(){
+string letter,uppercase_letter,lowercase_letter;
+cout<<"enter the letter"<<endl;
+cin>>letter;
+if(isupper(letter)){
+lowercase_letter = tolower(letter);
+cout<<lowercase_letter;
+}
+else if(islower(letter)){
+uppercase_letter = toupper(letter);
+cout<<uppercase_letter;
+}
+else{
+    cout<<" the character "<< letter <<" is not a letter ";
+}
+return 0;
+}
+
