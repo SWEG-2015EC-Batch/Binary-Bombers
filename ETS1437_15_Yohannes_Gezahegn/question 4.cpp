5cec4b76f8d66f3c35125970899596e920f73165
YohannesGezahegn <149233041+YohannesGezahegn@users.noreply.github.com>
2023-11-18 09:54:49 +0300

    Update question 4.cpp

--- ETS1437_15_Yohannes_Gezahegn/question 4.cpp
+++ ETS1437_15_Yohannes_Gezahegn/question 4.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  double x,y,z;
+  cout<<"insert number for x"<< endl;
+  cin>> x;
+  cout<<"insert number for y"<< endl;
+  cin>> y;
+  z = pow(x, y);
+  cout<<"the power of y to the base x is "<< z <<endl;
+
+return 0;
+}
+
+
+
