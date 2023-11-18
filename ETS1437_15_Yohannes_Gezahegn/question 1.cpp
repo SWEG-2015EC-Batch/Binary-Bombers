d03be0784430ae007ca509cb77daa0af11b9cd7e
YohannesGezahegn <149233041+YohannesGezahegn@users.noreply.github.com>
2023-11-18 09:44:02 +0300

    Update question 1.cpp

--- ETS1437_15_Yohannes_Gezahegn/question 1.cpp
+++ ETS1437_15_Yohannes_Gezahegn/question 1.cpp
@@ -1,1 +1,14 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+float BMI,weight,height;
+cout<<"please insert height"<<endl;
+cin>>height;
+cout<<"please insert weight"<<endl;
+cin>>weight;
+BMI=weight/(height*height);
+cout<<"BMI = "<< BMI <<endl;
+return 0;
+}
