a3262c10df2b2b311ab471aeea1236f3fb460175
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2023-11-19 08:48:37 -0800

    Update question 2, the miles problem.cpp

--- ETS1352_15_Yabets_Alelign/question 2, the miles problem.cpp
+++ ETS1352_15_Yabets_Alelign/question 2, the miles problem.cpp
@@ -8,7 +8,7 @@ int main()
 float fueltank, MilesPerGallons;
 cout<<"Please enter the holding capacity of the automobile fueltank in gallons: "<<endl;
 cin>>fueltank;
-cout<<"Please enter the traveling capacity of the automoblie fuel tank in miles: "<<endl;
+cout<<"Please enter the traveling capacity of the automoblie fueltank in miles: "<<endl;
 cin>>MilesPerGallons;
 cout<<"the number of miles the automobile can be driven without refueling is"<<fueltank*MilesPerGallons<<" miles."<<endl;
 return 0;
