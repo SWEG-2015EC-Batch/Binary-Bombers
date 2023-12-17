1d214cf2b7dd75ea445c041517cd5dc1968765dd
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2023-12-17 13:20:15 -0800

    Create full pyramid.cpp

---
+++ ETS1352_15_Yabets_Alelign/Printing patterns/full pyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows=6;
+    for (int i = 1; i <= rows; ++i) {
+        for (int space = 1; space <= rows - i; ++space) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            cout << "* ";
+        }
+    cout <<endl;
+    }
+
+    return 0;
+}
+
