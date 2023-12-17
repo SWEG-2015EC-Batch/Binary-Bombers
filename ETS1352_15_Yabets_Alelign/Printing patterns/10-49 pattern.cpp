e10852d160e679d001fd44207170de0994e96737
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2023-12-17 13:16:34 -0800

    Create 10-49 pattern.cpp

---
+++ ETS1352_15_Yabets_Alelign/Printing patterns/10-49 pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int length, width;
+    cout << "Enter the length: ";
+    cin >> length;
+    cout << "Enter the width: ";
+    cin >> width;
+    if (length <= 0 || width <= 0) {
+        cout << "Invalid input. Please enter positive values for length and width.\n";
+        return 1;
+
+    int initialNumber = 10;
+    for (int i = 0; i < length; ++i) {
+        for (int j = 0; j < width; ++j) {
+            cout << initialNumber << " ";
+            ++initialNumber;
+            if (initialNumber > 49) {
+                break;
+            }
+        }
+        cout << "\n";
+
+        if (initialNumber > 49) {
+            break;
+        }
+    }
+
+    return 0;
+}
+}
