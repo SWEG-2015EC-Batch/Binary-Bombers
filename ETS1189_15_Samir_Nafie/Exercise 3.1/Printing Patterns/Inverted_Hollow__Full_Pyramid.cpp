138aafe09def3ded2e996b9b3d7e0834b9aa07d5
Samir2022nafie <149320386+Samir2022nafie@users.noreply.github.com>
2023-12-18 11:45:00 +0300

    Create Inverted_Hollow__Full_Pyramid.cpp

---
+++ ETS1189_15_Samir_Nafie/Exercise 3.1/Printing Patterns/Inverted_Hollow__Full_Pyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows=6;
+    for (int i = rows; i >= 1; --i) {
+        for (int space = 1; space <= rows - i; ++space) {
+        cout << "  ";
+        }
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            if (j == 1 || j == 2 * i - 1 || i == rows) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout <<endl;
+    }
+    return 0;
+}
