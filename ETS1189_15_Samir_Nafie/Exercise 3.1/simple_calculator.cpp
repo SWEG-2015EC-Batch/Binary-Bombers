16df34d22803e702f6a1902c4e8f0154ea6639c8
Samir2022nafie <149320386+Samir2022nafie@users.noreply.github.com>
2023-12-18 09:06:16 +0300

    Update simple_calculator.cpp

--- ETS1189_15_Samir_Nafie/Exercise 3.1/simple_calculator.cpp
+++ ETS1189_15_Samir_Nafie/Exercise 3.1/simple_calculator.cpp
@@ -1,41 +1,55 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main()
+{
+    do{
+    int num1,num2,result;
+    char op;
+    cout<<"enter num1"<<endl;
+    cin>>num1;
+    cout<<"enter operator either of (+,-,*,/,%)"<<endl;
+    cin>>op;
+    cout<<"enter num2"<<endl;
+    cin>>num2;
 
-int main() {
-    char operation;
-    double num1, num2, result;
-
-    // Display user interface
-    cout << "Simple Calculator" << endl;
-    cout << "Enter two numbers and an operation (+, -, *, /): ";
-    cin >> num1 >> num2 >> operation;
-
-    // Perform calculation based on operation
-    switch(operation) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0) {
-                result = num1 / num2;
-            } else {
-                cout << "Error: Division by zero" << endl;
-                return 1;
-            }
-            break;
-        default:
-            cout << "Error: Invalid operation" << endl;
-            return 1;
+    switch(op){
+ case '+':
+    result=num1+num2;
+    break;
+ case'-':
+    result=num1-num2;
+    break;
+ case'*':
+    result=num1*num2;
+    break;
+ case'/':
+    if(num2!=0){
+        result=num1/num2;
+    }else{
+    cout<<"ERROR; can't be divided by zero"<<endl;
     }
+    break;
 
-    // Display the result
-    cout << "Result: " << result << endl;
+     case'%':
+     if(num2!=0){
+        result=num1 % num2;
+     }
+    else{
 
+        cout<<"error can't calculate module with zero divisor "<<endl;
+    }
+    break;
+
+    default:
+        cout<<"invalid operator"<<endl;
+    }
+    cout<< "RESULT= "<<result<<endl;
+    char choice;
+    cout<<"do you want to continue or not ? to continue press any key except '0' "<<endl;
+    cin>>choice;
+    if(choice='0'){
+        break;
+    }
+    }while(true);
     return 0;
 }
