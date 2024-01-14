355ffdfed5d41cde9e28225d64ec999bb37de28a
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2024-01-14 04:59:31 -0900

    Create 2D practical exercise Q3.cpp

---
+++ ETS1352_15_Yabets_Alelign/Activity 4.2/2D practical exercise Q3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void printScoreLevel(double totalScore);
+int main() 
+{
+    double test;
+    double assignment;
+    double quiz;
+    double project;
+    double final;
+cout << "Enter exam scores (out of 100):\n";
+cout << "Enter Test score: ";
+cin >> test;
+cout << "Enter Assignment score: ";
+cin >> assignment;
+cout << "Enter Quiz score: ";
+cin >> quiz;
+cout << "Enter Project score: ";
+cin >> project;
+cout << "Enter Final score: ";
+cin >> final;
+
+    double totalMark = test + assignment + quiz + project + final;
+cout << "\nExam Scores and Total Mark:\n";
+cout << setw(15) << "Exam Type" << setw(15) << "Score" << "\n";
+cout << "---------------------------------\n";
+cout << setw(15) << "Test" << setw(15) << test << "\n";
+cout << setw(15) << "Assignment" << setw(15) << assignment << "\n";
+cout << setw(15) << "Quiz" << setw(15) << quiz << "\n";
+cout << setw(15) << "Project" << setw(15) << project << "\n";
+cout << setw(15) << "Final" << setw(15) << final << "\n";
+cout << "---------------------------------\n";
+cout << setw(15) << "Total Mark" << setw(15) << totalMark << "\n";
+cout << setw(15) << "Score Level" << setw(15);
+printScoreLevel(totalMark);
+cout << "\n";
+
+    return 0;
+}
+
+
+void printScoreLevel(double totalScore) {
+    if (totalScore >= 80) {
+        cout << "Excellent";
+    } else if (totalScore >= 60) {
+        cout << "Very Good";
+    } else if (totalScore >= 50) {
+        cout << "Fair";
+    } else if (totalScore >= 40) {
+        cout << "Poor";
+    } else {
+        cout << "Fail";
+    }
+}
